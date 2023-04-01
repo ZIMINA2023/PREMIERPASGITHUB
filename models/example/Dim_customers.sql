c2b3c1244a1b1b3710e73f0cb07950d0b4e4741a
mamadou.h.ndiaye@gmail.com <mamadou.h.ndiaye@gmail.com>
2023-04-01 14:09:41 +0000

    test swith vue vers table et utilisation de la fonction reference

--- models/example/Dim_customers.sql
+++ models/example/Dim_customers.sql
@@ -2,22 +2,19 @@
     materialized="table"
 )}}
 
-
 with customers as (
-select c_custkey, c_name,c_address, c_phone, c_nationkey 
-
-from REF_CLIENT.ODS.DIM_CUST
+select * from {{ref('dim_client')}}
 ),
 
 orders as (
-select * from REF_CLIENT.ODS.O_ORDER o
+select * from {{ref('dim_commande')}}
 ),
 
 cutomers_order as (
-select c.c_custkey as id,  o_orderkey, sum(o_totalprice) totalprice
+select o_custkey as id,  o_orderkey, sum(o_totalprice) totalprice
 
-from REF_CLIENT.ODS.DIM_CUST c join REF_CLIENT.ODS.O_ORDER o on c.c_custkey=o.o_custkey
-group by c.c_custkey,o_orderkey
+from orders 
+group by o_custkey, o_orderkey
 ),
 
 final as (
